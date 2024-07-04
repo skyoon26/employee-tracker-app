8d6fcf03c50faf5daf17e530163e1dddad46e94e
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2024-07-04 17:23:49 -0500

    Create dashboard and no match/404 components

--- employee-tracker-ui/src/App.jsx
+++ employee-tracker-ui/src/App.jsx
@@ -1,10 +1,17 @@
-import Header from './pages/header/Header'
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/dashboard/Dashboard';
+import Header from './pages/header/Header';
+import NoMatch from './pages/noMatch/NoMatch';
 
 function App() {
 
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard/>} />
+        <Route path="*" element={<NoMatch/>} />
+      </Routes>
     </>
 
   )
