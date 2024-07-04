33a7e99e67676414fcec80179bb18eab0e91c551
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2024-07-03 22:52:46 -0500

    Add api

---
+++ employee-tracker-api/src/main/java/com/dchae/employeetrackerapp/employee_tracker_api/EmployeeTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.dchae.employeetrackerapp.employee_tracker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTrackerApiApplication.class, args);
+	}
+
+}
---
+++ employee-tracker-api/src/test/java/com/dchae/employeetrackerapp/employee_tracker_api/EmployeeTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dchae.employeetrackerapp.employee_tracker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
