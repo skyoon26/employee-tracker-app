862ab3674a21d13b99b2472361ff639f24c4d1fa
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2024-07-05 17:19:48 -0500

    Call update employee api from react ui

--- employee-tracker-ui/src/pages/employee/UpdateUser.jsx
+++ employee-tracker-ui/src/pages/employee/UpdateUser.jsx
@@ -1,12 +1,14 @@
 import './UpdateUser.css'
 import { useEffect, useState } from 'react'
 import { Button, Form } from 'react-bootstrap'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const UpdateUser = () => {
 
   // Need to get the id from the params:
   const {id} = useParams();
+
+  const navigate = useNavigate();
 
   // Store data of the form using state:
   const [formData, setFormData] = useState({
@@ -40,11 +42,33 @@ const UpdateUser = () => {
     fetchEmployee();
   }, [id])
 
+  // This method calls the updated api
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`http://localhost:8080/api/employee/${id}`, {
+        method: 'PATCH',
+        headers:{
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+      console.log("User updated: ", data);
+
+      navigate(`/`);
+    } catch (error) {
+      console.error("Error updating user: ", error.message);
+    }
+  }
+
   return (
     <>
     <div className="center-form">
       <h1>Edit Employee</h1>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group controlId="formBasicName">
           <Form.Control 
             type="text"
